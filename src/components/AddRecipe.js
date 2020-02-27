b7318737c9b92d6a10707162d43ca705d37474b9
kkdev211 <kkdev211@users.noreply.github.com>
2020-02-27 00:09:04 -0500

    testing materials-ui

---
+++ src/components/AddRecipe.js
@@ -0,0 +1,76 @@
+
+//basic form
+//recipe_image
+//title
+//description
+//ingredients- list
+//directions
+//notes
+//author
+//button to submit/post
+
+// export default AddRecipe;
+// import React, { useState, useEffect } from "react";
+// const AddRecipe = props => {
+//     return (
+//         <div className="add-recipe-form">
+//             <p>This will be the recipe form!</p>
+//         </div>
+//     );
+    
+// }
+
+// export default AddRecipe;
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function AddRecipe() {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
