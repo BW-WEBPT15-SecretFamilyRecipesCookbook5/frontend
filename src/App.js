70c4906890200b207ef74920be3fb699c5798e4d
Kyle Pace <kpace2296@gmail.com>
2020-02-26 22:27:22 -0600

    setting up routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Login from './components/Login';
+import {Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Route exact path='/login' component={Login}/>
+      {/* <Route exact path='/login' component={Register}/> */}
     </div>
   );
 }
