149352202a131b1bdeb5ee93c7caa4a67c9a26e5
KK DeVuyst <56946693+kkdev211@users.noreply.github.com>
2020-02-27 21:52:29 -0500

    Revert "Kk devuyst"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { useState } from 'react';
-// import './App.css';
-import AddRecipe from "./components/AddRecipe"
-import SearchBar from "./components/SearchBar"
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <SearchBar />
-      <br></br>
-      <AddRecipe />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
