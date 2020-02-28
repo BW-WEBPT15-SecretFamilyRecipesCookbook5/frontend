969dfa4211e39cf6eec33558958b039a0897673d
Kyle Pace <kpace2296@gmail.com>
2020-02-27 22:02:38 -0600

    working on header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Login from './components/Login';
 import Register from './components/Register';
 import Header from './components/header';
 import {Route, Link} from 'react-router-dom';
-
-import { useState } from 'react';
 import AddRecipe from "./components/AddRecipe"
 import SearchBar from "./components/SearchBar"
 
@@ -13,14 +11,11 @@ import SearchBar from "./components/SearchBar"
 function App() {
   return (
     <div className="App">
-
       <Header />
       <Route exact path='/login' component={Login}/>
       <Route exact path='/register' component={Register}/>
-      <SearchBar />
-      <br></br>
-      <AddRecipe />
-
+      <Route exact path='/searchbar' component={SearchBar}/>
+      <Route exact path='/addrecipe' component={AddRecipe} />
     </div>
   );
 }
