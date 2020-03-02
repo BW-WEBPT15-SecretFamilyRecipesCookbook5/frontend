f440721f0607451f721d733b19155af2409b9117
kkdev211 <kkdev211@users.noreply.github.com>
2020-03-01 21:45:41 -0500

    add recipe form

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,72 +1,102 @@
  
-import React, {UseState, useEffect} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+import { withFormik, Form, Field } from "formik";
+import * as yup from "yup"; 
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: 200,
-    },
-  },
-}));
-
-export default function AddRecipe() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState('Controlled');
-
-  const handleChange = event => {
-    setValue(event.target.value);
+const RecipeForm = ({ touched, errors, status }) => {
+   
+    const [recipe, setRecipe] = useState({});
+  
+    useEffect(() => {
+      status && setRecipe(status);
+     }, [status]);
+  
+    return (
+      <div className="recipe-form">
+        <Form>
+        <label>
+            Title:
+            <Field type="text" name="title" placeholder="title" />
+          </label>
+          <label>
+            Author:
+            <Field type="text" name="author" placeholder="author" />
+          </label>
+          <label>
+            Description:
+            <Field type="text" name="description" placeholder="description" />
+            {touched.description && errors.description && (
+              <p className="errors">{errors.description}</p>
+            )}
+          </label>
+          <label>
+            Ingredients:
+            <Field type="text" name="ingredients" placeholder="ingredients" />
+          </label>
+          <label>
+            Directions:
+            <Field type="text" name="directions" placeholder="directions" />
+          </label>
+          <label>
+            Category:
+            <Field component="select" className="category-select" name="category">
+              <option>Choose an Option</option>
+              <option value="appetizer">Appetizer</option>
+              <option value="entree">Entree</option>
+              <option value="dessert">Dessert</option>
+              <option value="side">Side</option>
+            </Field>
+          </label>
+          <br></br>
+          <label>
+            Notes:
+            <Field as="textarea" type="text" name="notes" placeholder="Notes" />
+          </label>
+          <button>Save Recipe</button>
+        </Form>
+        {recipe.title && (
+          <ul key={recipe.id}>
+            <li>Title: {recipe.title}</li>
+            <li>Author: {recipe.size}</li>
+            <li>Description: {recipe.description}</li>
+            <li>Ingredients: {recipe.ingredients}</li>
+            <li>Directions: {recipe.directions}</li>
+            <li>Category: {recipe.category}</li>
+            <li>Notes: {recipe.notes}</li>
+          </ul>
+        )}
+      </div>
+    );
   };
+  
+const FormikRecipeForm = withFormik({
+    mapPropsToValues: props => ({
+      title: props.title || "",
+      author: "",
+      description: "",
+      ingredients: "",
+      directions: "",
+      category: "",
+      notes: ""
+    }),
+    validationSchema: yup.object().shape({
+      species: yup
+        .string()
+        .required("This is the recipe field and it is required!")
+    }),
+    handleSubmit: (values, { resetForm, setStatus }) => {
+      // console.log("Submitting!", formikBag)
+      // POST body === {}
+      axios
+        // .post("https://reqres.in/api/users/", values)
+        .then(response => {
+          console.log(response);
+          setStatus(response.data);
+          resetForm();
+        })
+        .catch(err => console.log(err.response));
+    }
+  })(RecipeForm);
 
-  return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        <TextField
-          id="outlined-multiline-static"
-          label="Title"
-          multiline
-          rows="4"
-          defaultValue="Default Value"
-          variant="outlined"
-        />
-      </div>
-      <div>
-        <TextField
-          id="outlined-multiline-static"
-          label="Description"
-          multiline
-          rows="4"
-          defaultValue="Enter Description"
-          variant="outlined"
-        />
-      </div>
-      <div>
-        <TextField
-          id="outlined-multiline-static"
-          label="Ingredients"
-          multiline
-          rows="4"
-          defaultValue="Enter Ingredients"
-          variant="outlined"
-        />
-      </div>
-    
-      <div>
-        <TextField
-          id="outlined-multiline-static"
-          label="Directions"
-          multiline
-          rows="4"
-          defaultValue="Enter Directions"
-          variant="outlined"
-        />
-      </div>
-      <Button variant="contained" color="primary">
-       Submit
-      </Button>
-    </form>
-  );
-}
+export default FormikRecipeForm;
